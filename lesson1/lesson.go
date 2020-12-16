63b9c691354a70ef0aa84a8bd75e6fc57b072e24
yuta.hasegawa <yhsgw8023@gmail.com>
2020-12-16 22:58:45 +0900

    add slice

--- lesson1/lesson.go
+++ lesson1/lesson.go
@@ -137,4 +137,24 @@ func main() {
 	var b []int = []int{100, 200}
 	b = append(b, 300)
 	fmt.Println(b) // [100, 200, 300] →スライス
+
+	// スライス
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)      // [1 2 3 4 5 6]
+	fmt.Println(n[2])   // 3
+	fmt.Println(n[2:4]) // [3 4]
+	fmt.Println(n[:2])  // [1 2]
+	fmt.Println(n[2:])  // [3 4 5 6]
+	fmt.Println(n[:])   // [1 2 3 4 5 6]
+
+	n[2] = 100
+	fmt.Println(n) // [1 2 100 4 5 6]
+
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+		[]int{6, 7, 8},
+	}
+	fmt.Println(board) // [[0 1 2][3 4 5][6 7 8]]
+
 }
