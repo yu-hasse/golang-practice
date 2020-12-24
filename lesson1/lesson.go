f5a42fc331287683477c819442813f611ab450f1
yuta.hasegawa <yhsgw8023@gmail.com>
2020-12-24 22:05:02 +0900

    add function

--- lesson1/lesson.go
+++ lesson1/lesson.go
@@ -215,4 +215,30 @@ func main() {
 	fmt.Println(cbyte)         // [72 73]
 	fmt.Println(string(cbyte)) // HI
 
+	r1, r2 := add(10, 20)
+	fmt.Println(r1, r2)
+
+	r3 := calc(100, 2)
+	fmt.Println(r3)
+
+	inf := func(x int) {
+		fmt.Println("innner func", x)
+	}
+	inf(1)
+
+	func(x int) {
+		fmt.Println("innner func", x)
+	}(1)
+}
+
+// 関数
+func add(x int, y int) (int, int) {
+	fmt.Println("add function")
+	return x + y, x - y
+
+}
+
+func calc(price, item int) (result int) {
+	result = price * item
+	return result
 }
