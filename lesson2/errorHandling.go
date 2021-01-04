fb320a3df9c1299fd42e43cecab1f617125b3947
yuta.hasegawa <yhsgw8023@gmail.com>
2021-01-04 22:34:59 +0900

    error handling

---
+++ lesson2/errorHandling.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./errorHandling.go")
+	if err != nil {
+		log.Fatal("Error!")
+	}
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal("Error")
+	}
+	fmt.Println(count, string(data))
+
+	if err = os.Chdir("test"); err != nil {
+		log.Fatal("Error")
+	}
+}
