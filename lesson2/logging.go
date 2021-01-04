61a5b231af1991758cd9fcd7075b2eeace3d9f2b
yuta.hasegawa <yhsgw8023@gmail.com>
2021-01-04 20:53:07 +0900

    add logging

---
+++ lesson2/logging.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	loggingSettings("test.log")
+	log.Println("logging")
+	log.Printf("%T %v", "test", "test")
+
+	log.Fatalf("%T %v", "test", "test")
+	log.Fatalln("error!")
+
+}
+
+func loggingSettings(logFile string) {
+	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
+}
